f3ddcf8a96bd402f0c1c7e1ae32aa8b3d4f33acc
--replace-all <sawczak@tdchristian.ca>
2026-02-13 11:56:47 -0500

    basic structure

--- senior/S3.py
+++ senior/S3.py
@@ -16,7 +16,14 @@ changes = []
 for _ in range(Q):
     vals = input()
     kind, i, new = (int(v) for v in vals.split())
-    changes.append([kind, i new])
+    changes.append((kind, i, new))
 
+def paint_picture(i) -> int:
+    pass
 
-    
+total_prettiness = 0
+
+for i in range(1, Q + 1):
+    total_prettiness += paint_picture(i)
+
+print(paint_picture)
