20bd65557e3b715fc252e235fa69c5d932339f31
Luke Sawczak <luke@sawczak.com>
2026-02-13 23:33:55 -0500

    comments

--- senior/S3.py
+++ senior/S3.py
@@ -19,31 +19,31 @@ for _ in range(Q):
     changes.append((int(v) for v in vals.split()))
 
 def find_optimal_change() -> tuple[int, int]:
+    """
+    Check for an optimal change of the colour of one pen.
+    Return a tuple [i, oc, nc] where the colour of the pen
+    at index i is currently oc and should be changed to nc.
+    If no change is desirable, oc == nc.
 
-    # Check if there's an optimization change to make.
-    # We can change the colour of one pen.
-
-    # A change is desirable when a given colour (A)
-    # will gain more prettiness than the loss to the other colour (B).
-    # This would occur whenever A's maximum prettiness is lower than
-    # a prettiness available to B that is less than B's maximum prettiness.
-    # For example, if A has [2, 3] and B has [4, 6], if we swap the pen
-    # with 4 prettiness to A, our total prettiness is higher.
+    A change is desirable when a given colour A
+    will gain more prettiness than the loss to the other colour B.
+    This would occur whenever A's maximum prettiness is lower than
+    a prettiness available to B that is less than B's maximum prettiness.
+    For example, if A has [2, 3] and B has [4, 6], if we swap the pen
+    with 4 prettiness to A, our total prettiness is higher.
     
-    # To make it optimal, however, presumably we need to find said switch
-    # that maximizes the total gain; that is, where there is the greatest
-    # difference between the highest P of any colour A and the non-highest
-    # P of any colour B.
-
-    # Check for an optimal change. Return a tuple [i, oc, nc] where the colour
-    # at index i is currently oc and should be changed to nc.
-    # If no change is desirable, oc == nc.
+    To make it optimal, however, presumably we need to find said change
+    that maximizes the total gain; that is, where there is the greatest
+    difference between the highest prettiness of any colour A and the
+    second-highest prettiness of any colour B.
+    """
 
     # Defaults that won't affect anything if they don't get replaced
     change_i = 0
     change_oc = colours[0]
     change_nc = colours[0]
 
+    # "best seen yet" algorithm
     greatest_difference = 0
 
     # Try each colour's best one
@@ -63,8 +63,6 @@ def find_optimal_change() -> tuple[int, int]:
                 continue
 
             # Remove max to avoid swapping with it
-            # TODO I *think* this is good? That swapping with the max would not make sense?...   
-
             other_best = max(other_prettinesses)
             i_other_best = other_prettinesses.index(other_best)
             other_prettinesses.remove(other_best)
@@ -75,6 +73,7 @@ def find_optimal_change() -> tuple[int, int]:
             i_prettiness = other_prettinesses.index(other_best)
             i_colour = colour_indices[i_prettiness]
 
+            # Check the difference.
             difference = other_best - my_best
             if difference > greatest_difference:
                 greatest_difference = difference
@@ -85,9 +84,10 @@ def find_optimal_change() -> tuple[int, int]:
     return change_i, change_oc, change_nc
 
 def get_prettinesses_of_colour(target_colour: int) -> tuple[list[int]]:
-
-    # Gather the prettinesses of the pens of this colour
-    # Return them along with a parallel list of their indices
+    """
+    Gather the prettinesses of the pens of this colour.
+    Return them along with a parallel list of their indices in the list of pens.
+    """
 
     indices = []
     results = []
@@ -100,6 +100,14 @@ def get_prettinesses_of_colour(target_colour: int) -> tuple[list[int]]:
     return indices, results
 
 def paint_picture() -> int:
+    """
+    Paint the current picture and return its prettiness.
+
+    1. Check for an optimal change
+    2. For each colour, paint using the prettiest pen, summing its prettiness
+    3. Undo the optimal change
+    4. Return the summed prettiness
+    """
 
     # Get an optimal change. i, old colour, new colour
     change_i, change_oc, change_nc = find_optimal_change()
@@ -118,6 +126,8 @@ def paint_picture() -> int:
 
     # Return painting's prettiness
     return prettiness
+
+ # Actual program
 
 print(paint_picture())
 
